70289cefb4793a25906e8b7fc94a05f96f817f59
klimson <klimson@gitlab.com>
2018-10-13 16:52:18 +0200

    Implemented put/delete rest controller for category

--- src/main/java/pl/maniaq/library/controller/CategoryController.java
+++ src/main/java/pl/maniaq/library/controller/CategoryController.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.maniaq.library.exceptions.AuthorNotFoundException;
 import pl.maniaq.library.exceptions.BookExistException;
 import pl.maniaq.library.exceptions.CategoryExistException;
+import pl.maniaq.library.exceptions.CategoryNotFoundException;
+import pl.maniaq.library.model.Author;
 import pl.maniaq.library.model.BodyMessage;
 import pl.maniaq.library.model.Book;
 import pl.maniaq.library.model.Category;
@@ -71,6 +74,61 @@ public class CategoryController {
 
         } catch (IOException ex) {
             ex.printStackTrace();
+            body.setMessage("Perhaps you applied wrong format data.");
+            body.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+            response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(bodyMessageAssembler.getBodyJSON(body));
+        }
+
+        return response;
+    }
+
+    @RequestMapping(
+            value="/{id}",
+            method={RequestMethod.DELETE}
+    )
+    public ResponseEntity<String> deleteCategory(@PathVariable(value="id") Long id) throws IOException {
+        ResponseEntity<String> response;
+        BodyMessage body = new BodyMessage.Builder()
+                .setOperation(CrudOperations.DELETE)
+                .setModel(Category.class)
+                .build();
+
+        try {
+            categoryService.removeCategory(id);
+            response = ResponseEntity.status(HttpStatus.ACCEPTED).build();
+        } catch (CategoryNotFoundException e) {
+            e.printStackTrace();
+            body.setMessage(e.getMessage());
+            body.setStatus(HttpStatus.NOT_FOUND);
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(bodyMessageAssembler.getBodyJSON(body));
+        }
+
+        return response;
+    }
+
+    @RequestMapping(
+            value="",
+            method={RequestMethod.PUT},
+            consumes="application/json",
+            produces="application/json"
+    )
+    public ResponseEntity<String> updateCategory(@RequestBody Category category) throws IOException {
+        ResponseEntity<String> response;
+        BodyMessage body = new BodyMessage.Builder()
+                .setOperation(CrudOperations.PUT)
+                .setModel(Category.class)
+                .build();
+
+        try {
+            Category updatedCategory = categoryService.updateCategory(category);
+             response = ResponseEntity.ok(assembler.getModelObj(updatedCategory));
+        } catch (CategoryNotFoundException e) {
+            e.printStackTrace();
+            body.setMessage("Category does not exist.");
+            body.setStatus(HttpStatus.NOT_FOUND);
+            response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(bodyMessageAssembler.getBodyJSON(body));
+        } catch (IOException e) {
+            e.printStackTrace();
             body.setMessage("Perhaps you applied wrong format data.");
             body.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
             response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(bodyMessageAssembler.getBodyJSON(body));
